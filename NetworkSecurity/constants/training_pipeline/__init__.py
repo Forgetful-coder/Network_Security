338ee34e8b6f3183a833a025ce90d7a195970135
Forgetful-coder <aayushaggarwal243@gmail.com>
2025-01-11 14:18:49 -0600

    model trainer module

--- NetworkSecurity/constants/training_pipeline/__init__.py
+++ NetworkSecurity/constants/training_pipeline/__init__.py
@@ -61,3 +61,16 @@ DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.7
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+TRAINING_BUCKET_NAME = "networksecurity"
+
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from NetworkSecurity.components.data_ingestion import DataIngestion
 from NetworkSecurity.components.data_validation import DataValidation
 from NetworkSecurity.components.data_transformation import DataTransformation
+from NetworkSecurity.components.model_trainer import ModelTrainer
 from NetworkSecurity.Exceptions.exceptions import NetworksecurityException
 from NetworkSecurity.logging.logger import logging 
 from NetworkSecurity.entity.config_entity import (DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,
-                                                  DataTransformationConfig)
+                                                  DataTransformationConfig,ModelTrainerConfig)
 import sys
 
 
@@ -29,6 +30,13 @@ if __name__=='__main__':
         data_transformation_arti = data_transformation.initiate_data_transformation()
         print(data_transformation_arti)
         logging.info('Data Transformation Successful')
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(training_pipleine_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_arti)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+        
 
 
 
